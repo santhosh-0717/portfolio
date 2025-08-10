4b2d5ce427ec15bf361b147c7d279b4f8441fb2b
santhosh-0717 <gsanthosh037@gmail.com>
2025-08-10 22:24:00 +0530

    Revert "Update certificates.tsx"

--- components/certificates.tsx
+++ components/certificates.tsx
@@ -14,21 +14,13 @@ export default function Certificates() {
                 certLink="https://drive.google.com/file/d/14cRcnxyCQ_Lv2kk_lBgosP_R75S3irCv/preview"
             />
 
-       <<CertificateCard
-                companyName="Letsupgrade"
+       <CertificateCard
+                companyName="cisco"
                 recipientName="sai ram santhosh guttula"
-                courseName="python Bootcamp"
+                courseName="python essentials 2"
                 issueDate={new Date('2025-05-31')}
-                certificateId="LUEPYTMAY1251760"
-                certLink="https://drive.google.com/file/d/1xUwrd4O6yornR39GpFk6iKLg67cpVPiS/preview"
-                />
-               <CertificateCard
-                companyName="cicso"
-                recipientName="sai ram santhosh guttula"
-                courseName="Essentials 2"
-                issueDate={new Date('2025-06-08')}
-                certificateId="n"
-                certLink="https://drive.google.com/file/d/1hOP6-39QwNhx4HGHb_OTOAcL_SKsx4Wl/preview"
+                certificateId=""
+                certLink="https://drive.google.com/file/d/1hOP6-39QwNhx4HGHb_OTOAcL_SKsx4Wl/view?usp=sharing/preview"
 
             /> 
         </div>
