e5e01c700c15ef04343a8ee7f7440a734df6798e
santhosh-0717 <gsanthosh037@gmail.com>
2025-08-26 09:49:46 +0530

    Update certificates.tsx

--- components/certificates.tsx
+++ components/certificates.tsx
@@ -28,8 +28,15 @@ export default function Certificates() {
                 issueDate={new Date('2025-06-08')}
                 certificateId=""
                 certLink="https://drive.google.com/file/d/1hOP6-39QwNhx4HGHb_OTOAcL_SKsx4Wl/preview"
-
             /> 
+               <CertificateCard
+                companyName="lets upgrade "
+                recipientName="sai ram santhosh guttula"
+                courseName="GEN AI"
+                issueDate={new Date('2025-06-02')}
+                certificateId="LUEGENMAR1251066"
+                certLink="https://drive.google.com/file/d/1LoBiY7W5TNjL7i-VYmVNhTuO_A8IsDYN/preview"
+            />
         </div>
 
     );
